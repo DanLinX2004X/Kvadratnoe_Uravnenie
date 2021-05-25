057b4c6b4211c0d3543c6f7767e4244704a89327
DanLinX2004X <daniill02604@gmail.com>
2021-05-25 22:40:08 +0300

    release v2

--- KvadratnoeUravnenie.java
+++ KvadratnoeUravnenie.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class KvadratnoeUravnenie {
   public static void main(String[] args) {
 
-    double a,b,c,D,x1,x2;
+    double a,b,c,D,Di,x1,x2;
     //Создадим новый объект класса Scanner, он необходим для ввода данных в программу. Инициализируем этот объект класса входным потоком:
     Scanner in = new Scanner(System.in);
 
@@ -24,15 +24,18 @@ public class KvadratnoeUravnenie {
     if(D > 0){
       x1 = (-b - Math.sqrt(D)) / (a * 2);
       x2 = (-b + Math.sqrt(D)) / (a * 2);
-      System.out.println("Решение:"+x1 +" " + x2 );
+      System.out.println("Решение:" +x1 + " " + x2 );
       //случай, когда дискриминант равен нулю
     } else if (D == 0) {
-
       x1 = -b / (D * 2);
+      System.out.println("Решение:" + x1 );
       //случай, когда дискриминант отрицателен
     } else{
 
-      System.out.println("Нет действительных корней");
+      System.out.println("Нет действительных корней, зато есть комплексные числа");
+      x1 = -b / (2 * a);
+      Di = Math.sqrt(-D) / (2 * a);
+      System.out.println("Решение:"+ x1 + " ± " + Di + "i"  );
 
     }
   }
